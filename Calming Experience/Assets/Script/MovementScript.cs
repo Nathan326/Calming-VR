e5ed8f33a66833209020567bea0e925c4ae1be10
Nathan326 <50762422+Nathan326@users.noreply.github.com>
2019-05-18 17:23:44 +1000

    Sprinting
    Sprinting Added

--- Calming Experience/Assets/Script/MovementScript.cs
+++ Calming Experience/Assets/Script/MovementScript.cs
@@ -3,25 +3,48 @@ using System.Collections;
 
 public class MovementScript : MonoBehaviour
 {
+    float speed;
+    float sprintSpeed;
 
+    void Start()
+    {
+        speed = 5f;
+        sprintSpeed = 10f;
+    }
     void Update()
     {
         if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D)))
         {
-            transform.position = transform.position + Camera.main.transform.right * 5 * Time.deltaTime;
+            transform.position = transform.position + Camera.main.transform.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
         {
-            transform.position = transform.position + Camera.main.transform.right * -5 * Time.deltaTime;
+            transform.position = transform.position + Camera.main.transform.right * -speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.S)))
         {
-            transform.position = transform.position + Camera.main.transform.forward * -5 * Time.deltaTime;
+            transform.position = transform.position + Camera.main.transform.forward * -speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W)))
         {
-            transform.position = transform.position + Camera.main.transform.forward * 5 * Time.deltaTime;
+            transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
         }
 
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+
+        {
+            speed = sprintSpeed;
+        }
+        else
+
+        {
+            speed = speed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 5f;
+        }
     }
 }
