5ae624943097d0b47be5480b7a51681aabd2e43c
Nathan326 <50762422+Nathan326@users.noreply.github.com>
2019-05-18 15:20:32 +1000

    Base Project
    Base project for the experience

---
+++ Calming Experience/Assets/Script/MovementScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D)))
+        {
+            transform.position = transform.position + Camera.main.transform.right * 5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
+        {
+            transform.position = transform.position + Camera.main.transform.right * -5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.S)))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * -5 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W)))
+        {
+            transform.position = transform.position + Camera.main.transform.forward * 5 * Time.deltaTime;
+        }
+
+    }
+}
