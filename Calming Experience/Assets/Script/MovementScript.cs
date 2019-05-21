7d8450fce1c74112c1627e7ddfff1cf92e23f81e
Nathan326 <50762422+Nathan326@users.noreply.github.com>
2019-05-21 18:17:13 +1000

    Collisions

--- Calming Experience/Assets/Script/MovementScript.cs
+++ Calming Experience/Assets/Script/MovementScript.cs
@@ -6,30 +6,42 @@ public class MovementScript : MonoBehaviour
     LevelCamera flying;
     float speed;
     float sprintSpeed;
+    public Rigidbody rb;
 
     void Start()
     {
         flying = gameObject.GetComponent<LevelCamera>();
         speed = 5f;
         sprintSpeed = 10f;
+        rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
         if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D)))
         {
+            rb.isKinematic = false;
             transform.position = transform.position + Camera.main.transform.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
         {
+            rb.isKinematic = false;
             transform.position = transform.position + Camera.main.transform.right * -speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.S)))
         {
+            rb.isKinematic = false;
             transform.position = transform.position + Camera.main.transform.forward * -speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W)))
         {
+            rb.isKinematic = false;
             transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+        }
+
+        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.W)) 
+        {
+            rb.isKinematic = true;
+            transform.position = transform.position;
         }
         if (flying.enabled == false)
         {
--- Calming Experience/Assets/Script/Noclip.cs
+++ Calming Experience/Assets/Script/Noclip.cs
@@ -14,6 +14,9 @@ public class Noclip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float x = transform.position.x;
+        float z = transform.position.z;
+        float y = 1;
         if (Input.GetKeyUp(KeyCode.N))
         {
             noclip.enabled = false; //enable the script when space is pressed
@@ -21,6 +24,14 @@ public class Noclip : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.M))
         {
             noclip.enabled = true; //disable the script when escape is pressed
+        }
+
+        if (noclip.enabled == false)
+        {
+            if(transform.position.y <= 0)
+            {
+                transform.position = new Vector3(x, y, z);
+            }
         }
     }
 }
